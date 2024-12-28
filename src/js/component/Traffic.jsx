837d0e40eae2ffd3bb2dba75e8991f3ebc84bd12
JonathanM1206 <jonathanmoesses@gmail.com>
2024-12-28 01:15:35 +0000

    traffic

---
+++ src/js/component/Traffic.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'  
+import '../component/index.css'
+
+
+const Traffic = () => { 
+  const [selectedcolor,setSelectedColor]=useState("red"); 
+
+  
+  
+  
+  
+  return (
+    <div className="traffic-light">
+			<div onClick={()=>setSelectedColor("red")} className={"Light red "+(selectedcolor === "red" ? " glow":"")}></div>
+			<div onClick={()=>setSelectedColor("yellow")} className={"Light yellow"+(selectedcolor === "yellow" ? " glow":"")}></div>
+			<div onClick={()=>setSelectedColor("green")} className={"Light green"+(selectedcolor === "green" ? " glow":"")}></div>
+
+		</div>
+  )
+}
+
+export default Traffic
